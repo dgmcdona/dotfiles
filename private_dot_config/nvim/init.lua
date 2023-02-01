4e5843849fc34c2ceef715ce558e72f24dc76813
David McDonald <dgmcdona@uno.edu>
2023-01-31 22:56:05 -0600

    adds karabiner, updates zshrc, brewfile

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -285,8 +285,8 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
--- vim.cmd [[colorscheme gruvbox]]
-vim.cmd [[colorscheme onedark]]
+
+vim.cmd.colorscheme("onedark")
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
