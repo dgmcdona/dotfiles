551df7d2a9593b857fc9917c0cd158da4f051601
David McDonald <dgmcdona@uno.edu>
2023-10-22 23:32:17 -0500

    Move plugins to user-plugins dir

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -176,55 +176,7 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
-
-  -- terminals
-  {
-    'akinsho/toggleterm.nvim', 
-    version = "*", 
-    config = function()
-      require('toggleterm').setup({
-        open_mapping = '<C-,>',
-        direction = 'float',
-        float_opts = {
-          border = 'curved',
-        },
-      })
-    end
-  },
-
-  -- git
-  {
-    "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-  },
-
-  -- neotree
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    }
-  },
-
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'river_rat.plugins' },
 }, {})
 
 require('river_rat.options')
--- private_dot_config/nvim/lua/river_rat/plugins/init.lua
+++ private_dot_config/nvim/lua/river_rat/plugins/init.lua
@@ -2,4 +2,39 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- terminals
+  {
+    'akinsho/toggleterm.nvim', 
+    version = "*", 
+    config = function()
+      require('toggleterm').setup({
+        open_mapping = '<C-,>',
+        direction = 'float',
+        float_opts = {
+          border = 'curved',
+        },
+      })
+    end
+  },
+
+  -- git
+  {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+  },
+
+  -- neotree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  },
+}
