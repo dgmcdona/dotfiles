a2628e74fd27f09e1e93fc68ce3bede907cbf839
David McDonald <dgmcdona@uno.edu>
2023-10-25 00:00:49 -0500

    adds neo-tree, toggleterm, conform

--- private_dot_config/nvim/lua/river_rat/plugins/init.lua
+++ private_dot_config/nvim/lua/river_rat/plugins/init.lua
@@ -5,8 +5,8 @@
 return {
   -- terminals
   {
-    'akinsho/toggleterm.nvim', 
-    version = "*", 
+    'akinsho/toggleterm.nvim',
+    version = "*",
     config = function()
       require('toggleterm').setup({
         open_mapping = '<C-,>',
@@ -23,7 +23,7 @@ return {
     "kdheepak/lazygit.nvim",
     -- optional for floating window border decoration
     dependencies = {
-        "nvim-lua/plenary.nvim",
+      "nvim-lua/plenary.nvim",
     },
   },
 
@@ -35,6 +35,71 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
-    }
+    },
+    init = function()
+      vim.keymap.set('n', '<leader>tt', function()
+          local reveal_file = vim.fn.expand('%:p')
+          if (reveal_file == '') then
+            reveal_file = vim.fn.getcwd()
+          else
+            local f = io.open(reveal_file, "r")
+            if (f) then
+              f.close(f)
+            else
+              reveal_file = vim.fn.getcwd()
+            end
+          end
+          require('neo-tree.command').execute({
+            action = "focus",          -- OPTIONAL, this is the default value
+            source = "filesystem",     -- OPTIONAL, this is the default value
+            position = "left",         -- OPTIONAL, this is the default value
+            toggle = true,
+            reveal_file = reveal_file, -- path to file or folder to reveal
+            reveal_force_cwd = true,   -- change cwd without asking if needed
+          })
+        end,
+        { desc = "[T]oggle [T]ree" }
+      );
+    end
   },
+
+  -- formatting
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    -- Everything in opts will be passed to setup()
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = { { "prettierd", "prettier" } },
+        go = { "gofumpt" }
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      -- Customize formatters
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
+      },
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  }
 }
