dd87162856fc65f2553ee4672e8125ac6a3fa3f2
David McDonald <dgmcdona@uno.edu>
2025-10-29 23:37:50 -0500

    refactor: Remove commented lines

--- private_dot_config/nvim/lua/river_rat/colorscheme.lua
+++ private_dot_config/nvim/lua/river_rat/colorscheme.lua
@@ -1,7 +1,6 @@
 local theme = os.getenv("COLORTHEME")
 
 if theme == "light" then
-  -- vim.cmd.colorscheme "catppuccin-latte"
   require("nightfox")
   vim.o.background = "light"
   vim.cmd.colorscheme("dawnfox")
@@ -9,10 +8,6 @@ elseif theme == "dark" then
   require("nightfox")
   vim.o.background = "dark"
   vim.cmd.colorscheme("nightfox")
--- vim.g.gruvbox_material_enable_italic = true
--- vim.g.gruvbox_material_enable_bold = true
--- vim.g.gruvbox_material_background = 'hard'
--- vim.o.background = "dark"
 else
   require("kanagawa")
   vim.cmd.colorscheme("kanagawa")
