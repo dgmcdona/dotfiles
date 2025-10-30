b73dc4bbd3e0d9d95116e0b75b2b6927f62b5f05
David McDonald <dgmcdona@uno.edu>
2025-10-30 00:10:08 -0500

    feature(wezterm): tabline, tab renaming

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -31,7 +31,7 @@ config.keys = {
 		}),
 	},
 	-- Split panes (tmux: prefix + " and prefix + %)
-	{ key = '-', mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 	{ key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 
 	-- Navigate panes (tmux: prefix + arrow keys or hjkl if you set it)
@@ -67,6 +67,22 @@ config.keys = {
 
 	-- Detach (not needed locally, but for muscle memory)
 	{ key = "d", mods = "LEADER", action = act.ShowLauncher },
+
+	{
+		key = "r",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 
 config.key_tables = {
@@ -81,5 +97,10 @@ config.key_tables = {
 		{ key = "Enter", action = "PopKeyTable" },
 	},
 }
+
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+
+tabline.setup()
+tabline.apply_to_config(config)
 
 return config
